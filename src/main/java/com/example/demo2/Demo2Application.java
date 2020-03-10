ffd5bc8cec119707a5e5a9d8db194b8aa31e9b18
xianglong <1391086179>
2020-03-10 19:42:18 +0800

    + ws

--- src/main/java/com/example/demo2/Demo2Application.java
+++ src/main/java/com/example/demo2/Demo2Application.java
@@ -4,7 +4,9 @@ import org.apache.ibatis.annotations.Mapper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@MapperScan(basePackages = "com.example.demo2.mapper")
 @SpringBootApplication
 public class Demo2Application {
 
